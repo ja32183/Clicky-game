1fbcbff5cb78f04b755bf1637b0608383e9777df
Jeff Amlani <ja32183@gmail.com>
2019-05-11 16:15:07 -0400

    Updates

---
+++ precache-manifest.0d1c0da0a2b0d42f3805be24cfe57c05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a21e2d8dcb05e525dbdd52c0e2619b",
+    "url": "/Clicky-game/index.html"
+  },
+  {
+    "revision": "d0f4211c87e4a478265b",
+    "url": "/Clicky-game/static/css/main.6e79c565.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "d0f4211c87e4a478265b",
+    "url": "/Clicky-game/static/js/main.e63318d4.chunk.js"
+  },
+  {
+    "revision": "cda0924bd23e3d9029fa",
+    "url": "/Clicky-game/static/js/runtime~main.952307d2.js"
+  }
+]);
